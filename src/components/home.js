1f3957823526e9f36646c3e0fbaee42da1fb41db
ChizhEM <elizavetka.chizh@gmail.com>
2022-08-02 16:59:19 +0300

    feast: add fucntional to project

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,55 @@
 import { Link } from "react-router-dom";
-import { Container, Home } from "./styles";
-import mingas from "../assets/mingaz_logo_blue.png";
+import {
+  Container,
+  Home,
+  Image,
+  Logo,
+  ContainerLogo,
+  Text,
+  MingasLogo, Arka,
+} from "./styles";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { useState } from "react";
+
 export const HomePage = () => {
+  const matches = useMediaQuery("(max-width: 768px)");
+  // const [isMobile, setIsMobile] = useState(false);
+  // const changeWindow = () => {
+  //   if (matches) {
+  //     setIsMobile(true);
+  //   } else {
+  //     setIsMobile(false);
+  //   }
+  // };
   return (
-    <Container>
-      <Home>
-        <h1>Home</h1>
-        <img alt={""} src={mingas} />
-        <nav>
-          <Link to="/api/groups">
-            Перейдите для просмотра турнирных таблиц
-          </Link>
-        </nav>
-      </Home>
-    </Container>
+    <>
+      {matches ? (
+        <Container>
+          <Home>
+            <Logo alt={""} src={require("../assets/logo.png")} />
+            <Link to="/api/groups">Просмотр турнирных таблиц</Link>
+            <Image>
+              <img alt={""} src={require("../assets/image.png")} />
+            </Image>
+          </Home>
+        </Container>
+      ) : (
+        <Container>
+          <Home>
+            <ContainerLogo>
+              <MingasLogo alt={""} src={require("../assets/mingas.png")} />
+              <Logo alt={""} src={require("../assets/logo.png")} />
+              <MingasLogo alt={""} src={require("../assets/tbzpng.png")} />
+            </ContainerLogo>
+            <Text src={require("../assets/text.png")} />
+            <Link to="/api/groups">Просмотр турнирных таблиц</Link>
+            <Image>
+              <Arka alt={""} src={require("../assets/arka.png")} />
+              <img alt={""} src={require("../assets/image.png")} />
+            </Image>
+          </Home>
+        </Container>
+      )}
+    </>
   );
 };
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 1rem;
-
+  width: 100%;
+  margin: 0 auto;
+  div {
+    margin: 0 auto;
+  }
   table {
     border-spacing: 0;
     border: 1px solid black;
@@ -31,6 +34,8 @@ export const Container = styled.div`
 
 export const Home = styled.div`
   display: flex;
+  max-width: 980px;
+  margin: 0 auto;
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -52,7 +57,7 @@ export const Home = styled.div`
     color: #0d4475;
     font-size: 18px;
     font-weight: 500;
-    width: 500px;
+    width: 40%;
     text-align: center;
     border-radius: 7px;
     text-decoration: none;
@@ -60,8 +65,7 @@ export const Home = styled.div`
     justify-content: center;
     align-items: center;
     height: 50px;
-    margin: 4% auto;
-    z-index: 3;
+    margin: 2% auto;
     cursor: pointer;
     border: 1px solid #0d4475;
     background-color: rgba(0, 0, 0, 0);
@@ -69,9 +73,48 @@ export const Home = styled.div`
     &:hover {
       transform: scale(1.1);
     }
+    @media (max-width: 840px) {
+      font-size: 16px;
+    }
+    @media (max-width: 600px) {
+      font-size: 14px;
+      height: 40px;
+    }
+    @media (max-width: 420px) {
+      font-size: 12px;
+      width: 50%;
+    }
   }
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+`;
+export const Image = styled.div`
+  position: absolute;
+  margin-bottom: 0;
+  padding-bottom: 0;
   img {
-    width: 30%;
-    margin: auto;
+    width: 100%;
   }
+`;
+export const Arka = styled.img`margin-top: 1100px;
+    width: 100%;`
+export const ContainerLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const Text = styled.img`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const MingasLogo = styled.img`
+  width: 60%;
+  height: 60%;
 `;
